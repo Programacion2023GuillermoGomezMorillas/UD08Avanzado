8ba02a3d02df2357225ab03599628d4a54b7a4e5
Guillemro <guigommor@alu.edu.gva.es>
2024-04-10 19:40:27 +0200

    Final

--- src/Conjunto.java
+++ src/Conjunto.java
@@ -1,3 +1,71 @@
-public class Conjunto {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
 
+public class Conjunto<T> {
+    private static final int TAM = 10;
+    private T[] array = (T[]) new Object[TAM];
+    int total = 0;
+
+    /**
+     *
+     * @param element a insertar
+     * @return devuelve true se ha insertado y fa
+     */
+    public boolean insert(T element) {
+        boolean isInsert = false;
+        for (int i =0; i<array.length;i++) {
+            if (array[i] == null) {
+                array[i] = element;
+                isInsert = true;
+                //utilizo el break para facilitar el trabajo y simplificar el codigo
+                break;
+            }
+        }
+        return isInsert;
+    }
+
+    public boolean delete(T element) {
+        boolean isTrue = false;
+        for (int i = 0; i< array.length; i++) {
+            if (element.equals(array[i])) {
+                array[i] = null;
+                isTrue = true;
+            }
+        }
+        return isTrue;
+    }
+    public T get(int pos){
+        return array[pos];
+    }
+    public int find(T element){
+        int posicion=-1;
+        for (int i =0; i<array.length; i++){
+            if (array[i].equals(element)){
+                posicion=i;
+            }
+        }
+        return posicion;
+    }
+
+    @Override
+    public String toString() {
+        return "Conjunto{" +
+                "array=" + Arrays.toString(array) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Conjunto<?> conjunto = (Conjunto<?>) o;
+        return Arrays.equals(array, conjunto.array);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(array);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Conjunto 1 de tipo String
+        Conjunto<String> conjunto1 = new Conjunto<>();
+
+        conjunto1.insert("jose");
+        conjunto1.insert("luis");
+        conjunto1.insert("pedro");
+        conjunto1.insert("maria");
+        conjunto1.insert("angel");
+        conjunto1.insert("manuel");
+        conjunto1.insert("sergio");
+        conjunto1.insert("alberto");
+        conjunto1.insert("carlos");
+        conjunto1.insert("antonio");
+        System.out.println(conjunto1.toString());
+
+        System.out.println("La posision de luis es: " + conjunto1.find("luis"));
+        System.out.println("En la posicion 8 tenemos a: " + conjunto1.get(8));
+
+
+
+        conjunto1.delete("jose");
+        conjunto1.delete("pedro");
+        conjunto1.delete("maria");
+        System.out.println(conjunto1.toString());
+
+        //Conjunto 2 de tipo Integer
+        Conjunto<Integer> conjunto2 = new Conjunto<>();
+
+        conjunto2.insert(0);
+        conjunto2.insert(1);
+        conjunto2.insert(2);
+        conjunto2.insert(3);
+        conjunto2.insert(4);
+        conjunto2.insert(5);
+        conjunto2.insert(6);
+        conjunto2.insert(7);
+        conjunto2.insert(8);
+        conjunto2.insert(9);
+        System.out.println(conjunto2.toString());
+
+        System.out.println("La posision del 2 es: " + conjunto2.find(2));
+        System.out.println("En la posicion 1 tenemos a: " + conjunto2.get(1));
+
+        conjunto2.delete(2);
+        conjunto2.delete(9);
+
+        System.out.println(conjunto2.toString());
+
+
+
+
     }
 }
